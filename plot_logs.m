6184829a3aebb99c3f0be0155975d161df563e9e
Kevin Robert Stravers <macocio@gmail.com>
2017-02-03 17:14:13 +0100

    Add a plot for the joystick input

--- plot_logs.m
+++ plot_logs.m
@@ -10,3 +10,14 @@ task_plotter(task_b, 'b) Pushing surge request');
 task_plotter(task_c, 'c) Port sway request');
 task_plotter(task_d, 'd) Starboard sway request');
 task_plotter(task_e, 'e) Clockwise yaw request');
+
+task_1 = load('task-1.csv');
+figure;
+hold on;
+task_1(:,3) = task_1(:,3) - task_1(1,3);
+plot(task_1(:,3), task_1(:,1));
+plot(task_1(:,3), task_1(:,2));
+title('Joystick input');
+legend('X','Y');
+xlabel('Time');
+ylabel('Magnitude');
